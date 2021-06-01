b345e27f7aa4478e9560b3f0570d96151d74ba31
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2021-06-02 00:18:07 +0530

    enable null safety

--- lib/flutter_insta.dart
+++ lib/flutter_insta.dart
@@ -4,16 +4,14 @@ import 'package:http/http.dart' as http;
 
 class FlutterInsta {
   String url = "https://www.instagram.com/";
-  String _followers, _following, _website, _bio, _imgurl,_username;
+  String? _followers, _following, _website, _bio, _imgurl, _username;
   // List of images from user feed
-  List<String> _feedImagesUrl;
+  List<String>? _feedImagesUrl;
 
   //Download reels video
   Future<String> downloadReels(String link) async {
     var linkEdit = link.replaceAll(" ", "").split("/");
-    var downloadURL = await http.get(
-        '${linkEdit[0]}//${linkEdit[2]}/${linkEdit[3]}/${linkEdit[4]}' +
-            "/?__a=1");
+    var downloadURL = await http.get(Uri.parse('${linkEdit[0]}//${linkEdit[2]}/${linkEdit[3]}/${linkEdit[4]}' + "/?__a=1"));
     var data = json.decode(downloadURL.body);
     var graphql = data['graphql'];
     var shortcodeMedia = graphql['shortcode_media'];
@@ -23,7 +21,7 @@ class FlutterInsta {
 
   //get profile details
   Future<void> getProfileData(String username) async {
-    var res = await http.get(Uri.encodeFull(url + username + "/?__a=1"));
+    var res = await http.get(Uri.parse(Uri.encodeFull(url + username + "/?__a=1")));
     var data = json.decode(res.body);
     var graphql = data['graphql'];
     var user = graphql['user'];
@@ -35,15 +33,16 @@ class FlutterInsta {
     _following = myfollowing['count'].toString();
     _website = user['external_url'];
     _imgurl = user['profile_pic_url_hd'];
-    _feedImagesUrl = user['edge_owner_to_timeline_media']['edges'].map<String>((image) => image['node']['display_url'] as String).toList();
-    this._username=username;
+    _feedImagesUrl =
+        user['edge_owner_to_timeline_media']['edges'].map<String>((image) => image['node']['display_url'] as String).toList();
+    this._username = username;
   }
 
-  String get followers => _followers;
+  String? get followers => _followers;
 
   get following => _following;
 
-  get username =>_username;
+  get username => _username;
 
   get website => _website;
 
@@ -51,5 +50,5 @@ class FlutterInsta {
 
   get imgurl => _imgurl;
 
-  List<String> get feedImagesUrl => _feedImagesUrl;
+  List<String>? get feedImagesUrl => _feedImagesUrl;
 }
